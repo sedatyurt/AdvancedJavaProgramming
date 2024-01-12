acac99829ae0d6adea33bef5af6130de0e646082
Sedat Yurt <sedatyurt34@gmail.com>
2024-01-13 00:24:05 +0300

    joker char added

--- src/main/java/wildcards/Main.java
+++ src/main/java/wildcards/Main.java
@@ -17,26 +17,26 @@ public class Main {
         List<Office> offices = new ArrayList();
         offices.add(new Office());
         offices.add(new Office());
-        //printBuildings(offices);
+        printBuildings(offices);
 
         //List of houses
         List<House> houses = new ArrayList();
         houses.add(new House());
         houses.add(new House());
-        //printBuildings(houses);
+        printBuildings(houses);
 
-        //addHouseToList(houses);
-        //addHouseToList(buildings);
+        addHouseToList(houses);
+        addHouseToList(buildings);
     }
 
-    static void printBuildings(List<Building> buildings){
+    static void printBuildings(List<? extends Building> buildings){
         for(int i = 0; i < buildings.size(); i++){
             System.out.println(buildings.get(i).toString() + " " + (i + 1));
         }
         System.out.println();
     }
 
-    static void addHouseToList(List<House> buildings){
+    static void addHouseToList(List<? super House> buildings){
         buildings.add(new House());
         System.out.println();
     }
