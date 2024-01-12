d2e359edf79dc717ceaa8cc076bfab06817d2460
Sedat Yurt <sedatyurt34@gmail.com>
2024-01-12 22:24:16 +0300

    GenericsExample added

--- src/main/java/org/example/GenericsExample.java
+++ src/main/java/org/example/GenericsExample.java
@@ -12,6 +12,14 @@ public class GenericsExample {
         names.add("Kelly");
         String name = (String) names.get(0);
         System.out.println("First name: " + name);
+        names.add(7);
+
+        //Example with Generics
+        List<String> names2 = new ArrayList();
+        names2.add("Kelly");
+        String name2 = names2.get(0);
+        System.out.println("First name: " + name2);
+        //names2.add(7); Throws error
 
     }
 }
