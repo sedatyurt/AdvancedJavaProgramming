23b00511009a3b46761f3025127f8c2daf002333
Sedat Yurt <sedatyurt34@gmail.com>
2024-01-12 23:15:07 +0300

    Varargs method fixed

--- src/main/java/org/example/Varargs.java
+++ src/main/java/org/example/Varargs.java
@@ -9,6 +9,7 @@ public class Varargs {
         String[] shopping = {"Bread", "Milk", "Eggs", "Bananas"};
         printShoppingList(item1, item2, item3);
         printShoppingList(shopping);
+        printShoppingList("Bread", "Milk", "Eggs", "Bananas");
     }
 
     private static void printShoppingList(String string1, String string2){
@@ -24,7 +25,7 @@ public class Varargs {
         System.out.println("3. " + string3);
     }
 
-    private static void printShoppingList(String[] items){
+    private static void printShoppingList(String... items){
         System.out.println("SHOPPING LIST");
         for(int i = 0; i < items.length; i++){
             System.out.println(i + 1 + ": " + items[i]);
