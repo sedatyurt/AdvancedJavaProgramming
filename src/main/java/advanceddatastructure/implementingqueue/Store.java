48317c7be39b954eb8de6bc98b53e18910fa3636
Sedat Yurt <sedatyurt34@gmail.com>
2024-01-13 12:53:30 +0300

    implementing queue with linked list example

---
+++ src/main/java/advanceddatastructure/implementingqueue/Store.java
@@ -0,0 +1,29 @@
+package advanceddatastructure.implementingqueue;
+
+import java.util.LinkedList;
+
+public class Store {
+
+    public static void main(String[] args) {
+
+        LinkedList<Customer> queue = new LinkedList();
+        queue.add(new Customer("Sally"));
+        queue.add(new Customer("Ben"));
+        queue.add(new Customer("Emma"));
+        queue.add(new Customer("Fred"));
+        System.out.println(queue);
+
+        serveCustomer(queue);
+        System.out.println(queue);
+
+        serveCustomer(queue);
+        System.out.println(queue);
+
+    }
+
+    static void serveCustomer(LinkedList<Customer> queue){
+
+        Customer customer = queue.poll();
+        customer.serve();
+    }
+}
