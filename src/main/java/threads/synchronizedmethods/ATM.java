54f30682edf49f1240eb9fdd8bfbde0b27423720
Sedat Yurt <sedatyurt34@gmail.com>
2024-01-17 13:16:08 +0300

    Synchronized example added

---
+++ src/main/java/threads/synchronizedmethods/ATM.java
@@ -0,0 +1,16 @@
+package threads.synchronizedmethods;
+
+public class ATM {
+
+    static synchronized void withdraw(BankAccount account, int amount){
+
+        int balance = account.getBalance();
+        if((balance - amount) < - account.getOverdraft()){
+            System.out.println("Transaction denied!");
+        }   else {
+            account.debit(amount);
+            System.out.println("$" + amount + " successfully withdraw");
+        }
+        System.out.println("Current balance: " + account.getBalance());
+    }
+}
