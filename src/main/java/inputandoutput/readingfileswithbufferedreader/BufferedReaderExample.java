64db2f4bfa603904fa93ae6c6e29a9c2c6ae6a00
Sedat Yurt <sedatyurt34@gmail.com>
2024-01-18 23:11:41 +0300

    reading files with BufferedReader example added

---
+++ src/main/java/inputandoutput/readingfileswithbufferedreader/BufferedReaderExample.java
@@ -0,0 +1,25 @@
+package inputandoutput.readingfileswithbufferedreader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BufferedReaderExample {
+
+    public static void main(String[] args) {
+
+        File myFile = new File("src/main/resources/example.txt");
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(myFile));
+            String line;
+            while ((line = reader.readLine()) != null){
+                System.out.println(line);
+            }
+
+        } catch (IOException e){
+
+        }
+    }
+}
